bffae206860aa536bb7f325286835ded3dee1775
Duncan Smith <RedGreenCode@users.noreply.github.com>
2017-01-19 09:12:54 -0800

    Copy for Example 2

---
+++ UWP-MVVM-EF-SQLite-2/UWPMain/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Windows.Foundation;
+using Windows.UI.ViewManagement;
+
+using UWPMain.ViewModels;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPMain
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+			ApplicationView.PreferredLaunchViewSize = new Size(700, 300);
+			ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+
+			Task = new TaskViewModel();
+		}
+
+		public TaskViewModel Task { get; set; }
+	}
+}
---
+++ UWP-MVVM-EF-SQLite-2/UWPMain/ViewModels/TaskViewModel.cs
@@ -0,0 +1,20 @@
+﻿using UWPMain.Models;
+
+namespace UWPMain.ViewModels
+{
+	public class TaskViewModel : NotificationBase<Task>
+	{
+		private Task _task;
+
+		public TaskViewModel()
+		{
+			_task = new Task();
+		}
+
+		public string Name
+		{
+			get { return This.Name; }
+			set { SetProperty(This.Name, value, () => This.Name = value); }
+		}
+	}
+}
