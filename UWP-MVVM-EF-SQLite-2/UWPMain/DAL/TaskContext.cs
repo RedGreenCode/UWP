75b81723aad1fce73d20a9534d0970f4ae6afafc
Duncan Smith <RedGreenCode@users.noreply.github.com>
2017-01-24 22:05:42 -0800

    EF and SQLite example

---
+++ UWP-MVVM-EF-SQLite-2/UWPMain/DAL/TaskContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using UWPMain.Models;
+
+namespace UWPMain.DAL
+{
+	public sealed class TaskContext : DbContext
+	{
+		internal DbSet<Task> Tasks { get; set; }
+
+		public TaskContext()
+		{
+			Database.Migrate();
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlite("Filename=Tasks.db");
+		}
+	}
+}
